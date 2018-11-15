af195f33dfa7b0389b7d411719b221bbc45d5f02
Dane Olsen <40639830+dno1995@users.noreply.github.com>
2018-11-15 16:03:58 -0500

    Update SQL Script.sql

--- SQL Script.sql
+++ SQL Script.sql
@@ -4,26 +4,29 @@ SET @film = 8
 ;
 
 SELECT 
-	Title
+    Title Film_Title
     , DAYNAME(bo1.BoxOfficeDay) Day
     , DATE_FORMAT(bo1.BoxOfficeDay, '%b. %e, %Y') Date
-    , DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1 Day_Number
-	, CEILING((DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1)/7) Week_Number
     , ROUND(bo1.GrossTakings) Gross_Takings
     , ROUND(((bo1.GrossTakings-bo2.GrossTakings)/bo2.GrossTakings)*100, 1) "%_Change_From_Prior_Day"
+    , ROUND(((bo1.GrossTakings-bo3.GrossTakings)/bo3.GrossTakings)*100, 1) "%_Change_From_Last_Week"
     , bo1.TheaterCount Theater_count
     , ROUND(bo1.GrossTakings/bo1.TheaterCount) Avg_Takings_Per_Theater
     , ROUND(SUM(GrossDate)) Gross_To_Date
+    , DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1 Day_Number
+    , CEILING((DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1)/7) Week_Number
 FROM Films f
 	LEFT JOIN BoxOffice bo1
 		ON f.FilmID=bo1.FilmID
 	LEFT JOIN BoxOffice bo2
 		ON bo1.BoxOfficeDay=DATE_ADD(bo2.BoxOfficeDay, INTERVAL 1 DAY)
+	LEFT JOIN BoxOffice bo3
+		ON bo1.BoxOfficeDay=DATE_ADD(bo3.BoxOfficeDay, INTERVAL 7 DAY)
 	LEFT JOIN (
 			SELECT 
 				b.FilmID
 				, b.BoxOfficeDay
-                , SUM(b.GrossTakings) GrossDate
+                		, SUM(b.GrossTakings) GrossDate
 			FROM BoxOffice b 
             GROUP BY b.FilmID, b.BoxOfficeday) ba
 		ON bo1.BoxOfficeDay >= ba.BoxOfficeDay
@@ -38,32 +41,35 @@ ORDER BY Day_Number
 
 -- This query results in a table with all the Star Wars movies in a single table, starting with the most recent one.
 SELECT 
-	Title
+    Title Film_Title
     , DAYNAME(bo1.BoxOfficeDay) Day
     , DATE_FORMAT(bo1.BoxOfficeDay, '%b. %e, %Y') Date
-    , DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1 Day_Number
-	, CEILING((DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1)/7) Week_Number
     , ROUND(bo1.GrossTakings) Gross_Takings
     , ROUND(((bo1.GrossTakings-bo2.GrossTakings)/bo2.GrossTakings)*100, 1) "%_Change_From_Prior_Day"
+    , ROUND(((bo1.GrossTakings-bo3.GrossTakings)/bo3.GrossTakings)*100, 1) "%_Change_From_Last_Week"
     , bo1.TheaterCount Theater_count
     , ROUND(bo1.GrossTakings/bo1.TheaterCount) Avg_Takings_Per_Theater
     , ROUND(SUM(GrossDate)) Gross_To_Date
+    , DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1 Day_Number
+    , CEILING((DATEDIFF(bo1.BoxOfficeDay, ReleaseDate)+1)/7) Week_Number
 FROM Films f
 	LEFT JOIN BoxOffice bo1
 		ON f.FilmID=bo1.FilmID
 	LEFT JOIN BoxOffice bo2
 		ON bo1.BoxOfficeDay=DATE_ADD(bo2.BoxOfficeDay, INTERVAL 1 DAY)
+	LEFT JOIN BoxOffice bo3
+		ON bo1.BoxOfficeDay=DATE_ADD(bo3.BoxOfficeDay, INTERVAL 7 DAY)
 	LEFT JOIN (
 			SELECT 
 				b.FilmID
 				, b.BoxOfficeDay
-                , SUM(b.GrossTakings) GrossDate
+                		, SUM(b.GrossTakings) GrossDate
 			FROM BoxOffice b 
             GROUP BY b.FilmID, b.BoxOfficeday) ba
 		ON bo1.BoxOfficeDay >= ba.BoxOfficeDay
 GROUP BY 
-	title
+    Title
     , f.FilmID
     , Date
-ORDER BY f.FilmID DESC, Day_Number
+ORDER BY f.filmID DESC, Day_Number
 ;
